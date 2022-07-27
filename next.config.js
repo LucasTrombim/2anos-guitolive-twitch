40b7e0fda2eddf2c3f298de4bc4c0269330aab6a
Lucas <lucas@plastikastudio.com.br>
2022-07-27 17:29:57 -0300

    Alterado nextconfig

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.woff2$/,
